fcf818faa017a3767c0733d1fd868ffcbff91a7b
Chiara Baldasarre Coppi <Chiara.Baldasarre0725@jala.university>
2025-11-14 09:41:00 -0300

    chore: Initial repository setup.

---
+++ src/main/java/com/unitutor/grupo3_unitutor/Grupo3UnitutorApplication.java
@@ -0,0 +1,13 @@
+package com.unitutor.grupo3_unitutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo3UnitutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo3UnitutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unitutor/grupo3_unitutor/Grupo3UnitutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitutor.grupo3_unitutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo3UnitutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
