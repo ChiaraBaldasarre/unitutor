eb728162b61b71cbae399a9b9e7f902e885cafd6
Lilian Laime Lucero <Lilian.Laime0725@jala.university>
2025-12-09 17:58:20 -0400

    UNI-017_fix_typo_and_resolve_merge_conflicts

---
+++ src/main/java/com/unitutor/grupo3_unitutor/repository/EnrollmentRepository.java
@@ -0,0 +1,14 @@
+package com.unitutor.grupo3_unitutor.repository;
+
+import com.unitutor.grupo3_unitutor.model.Enrollment;
+import com.unitutor.grupo3_unitutor.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+    @Query("SELECT e FROM Enrollment e WHERE e.student = :student ORDER BY e.session.startTime DESC")
+    List<Enrollment> findHistory(@Param("student") User student);
+}
